5d4e379775f8dac0e2b06c0f342fdce2500af7ce
Andy Bower <bower@bowerhaus.eu>
2012-09-10 20:27:07 +0100

    Initial source

---
+++ SceneManager/scene1.lua
@@ -0,0 +1,30 @@
+Scene1 = gideros.class(Sprite)
+
+function Scene1:init(t)
+	if t then
+		print("Scene1: ", t)
+	end	
+
+	self:addChild(Bitmap.new(Texture.new("gfx/scene1.jpg")))
+	
+	self:addEventListener("enterBegin", self.onTransitionInBegin, self)
+	self:addEventListener("enterEnd", self.onTransitionInEnd, self)
+	self:addEventListener("exitBegin", self.onTransitionOutBegin, self)
+	self:addEventListener("exitEnd", self.onTransitionOutEnd, self)
+end
+
+function Scene1:onTransitionInBegin()
+	print("scene1 - enter begin")
+end
+
+function Scene1:onTransitionInEnd()
+	print("scene1 - enter end")
+end
+
+function Scene1:onTransitionOutBegin()
+	print("scene1 - exit begin")
+end
+
+function Scene1:onTransitionOutEnd()
+	print("scene1 - exit end")
+end
